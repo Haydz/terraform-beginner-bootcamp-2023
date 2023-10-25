4547a4aa961dc947643634f2f20c82fd549d3104
Haydn Johnson <haydnit@gmail.com>
2023-10-25 19:55:01 -0400

    with file (#7)

--- bin/install_terraform_cli.sh
+++ bin/install_terraform_cli.sh
@@ -4,7 +4,6 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \
 gpg --dearmor | \
-
 sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
